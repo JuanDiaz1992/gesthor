4e716922d72c7deb1bd569cdbb442689b98c26b0
Juan Diaz <juannavegante2010@gmail.com>
2023-10-10 20:08:38 -0500

    actualizaciones

--- src/componentes/Contacto/FormularioContacto.jsx
+++ src/componentes/Contacto/FormularioContacto.jsx
@@ -43,7 +43,7 @@ function FormularioContacto (){
         };
 
 
-    emailjs.send('service_npopoñklñokafu', 'templatewdwawdasdaw_a92yuia',emailContent, 'ubhhuwpIV-yGaawdasdawPpNRY')
+    emailjs.send('service_npopafu', 'template_a92yuia',emailContent, 'ubhhuwpIV-yGPpNRY')
         .then((result) => {
             console.log(result.text);
         }, (error) => {
